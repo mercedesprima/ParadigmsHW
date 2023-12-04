61a4a149bd5a9c46f289c2a918c1d58af2be3725
Ekaterina_P <zazie25@yandex.ru>
2023-12-04 16:33:36 +0300

    lesson1 added

---
+++ lesson1/src/SortList.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class SortList {
+        public static void sortListImperative(int[] array) {
+            for (int i = 0; i < array.length - 1; i++) {
+                for (int j = 0; j < array.length - i - 1; j++) {
+                    if (array[j] < array[j + 1]) {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+
+        public static void sortListDeclarative(int[] array) {
+            Arrays.sort(array);
+            int left = 0;
+            int right = array.length - 1;
+            while (left < right) {
+                int temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        public static void main(String[] args) {
+            int[] numbers = {15, 62, 0, 4, 25};
+
+            sortListImperative(numbers);
+            System.out.println(Arrays.toString(numbers));
+
+            sortListDeclarative(numbers);
+            System.out.println(Arrays.toString(numbers));
+        }
+    }
