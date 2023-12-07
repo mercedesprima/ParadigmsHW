0810b98c405c3c346a1da7f122205b70876f6ea6
Ekaterina_P <zazie25@yandex.ru>
2023-12-07 22:59:07 +0300

    lesson 2 added

--- lesson2/src/MultiplicationTable.java
+++ lesson2/src/MultiplicationTable.java
@@ -10,8 +10,8 @@
             System.out.print("Введите число n: ");
             int n = scanner.nextInt();
 
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= 9; j++) {
+            for (int i = 1; i <= 9; i++) {
+                for (int j = 1; j <= n; j++) {
                     int result = i * j;
                     System.out.printf("%d * %d = %d \t", i,  j, result);
                 }
